e057098934b4250d4d24b974a5b2f566f4a38a50
Max <mlm20@ic.ac.uk>
2021-05-25 16:23:48 +0100

    basic js

---
+++ web-app/static/Index/main.js
@@ -0,0 +1,11 @@
+const add_candidate_button = document.getElementById("add-candidate-button");
+const start_election = document.getElementById("start-election-button");
+const candidate_input_text = document.getElementById("candidate-input-field");
+
+const cloneTemplate = function (id) {
+    return document.importNode(document.getElementById(id).content, true);
+};
+
+add_candidate_button.onclick = function () {
+    console.log(candidate_input_text.value);
+};
