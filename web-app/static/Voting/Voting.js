24361004772a817a20960a677367f792d9e9abdc
Max <mlm20@ic.ac.uk>
2021-05-31 15:29:49 +0100

    change

--- web-app/static/Voting/Voting.js
+++ web-app/static/Voting/Voting.js
@@ -1,1 +1,9 @@
 const candidate_list = JSON.parse(sessionStorage.candidate_list);
+const num_candidates = candidate_list.length;
+
+const see_results_button = document.getElementById("SeeResultsButton");
+
+see_results_button.onclick = function () {
+    console.log("Moved to results page");
+    location.href = "../Results/Results.html";
+};
