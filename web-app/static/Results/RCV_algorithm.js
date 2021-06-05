46198cfc25fab9911c3ddce44783d00ad83d9a50
Max <mlm20@ic.ac.uk>
2021-06-05 19:18:46 +0100

    setting up module

---
+++ web-app/static/Results/RCV_algorithm.js
@@ -0,0 +1,40 @@
+// Header
+const Algorithm = Object.create(null);
+
+// example data to debug with
+// DELETEME before commit
+const example_voting_data = [
+    {
+        "blue": 1,
+        "red": 3,
+        "purple": 2,
+        "yellow": 4
+    },
+    {
+        "blue": 2,
+        "red": 3,
+        "purple": 4,
+        "yellow": 1
+    },
+    {
+        "blue": 1,
+        "red": 2,
+        "purple": 4,
+        "yellow": 3
+    },
+    {
+        "blue": 3,
+        "red": 4,
+        "purple": 2,
+        "yellow": 1
+    }
+];
+
+
+
+
+
+
+
+// export module
+export default Object.freeze(Algorithm);
---
+++ web-app/static/Results/Results.js
@@ -0,0 +1,4 @@
+import Algorithm from "./RCV_algorithm";
+
+const candidate_list = JSON.parse(sessionStorage.candidate_list);
+const raw_voting_data = JSON.parse(sessionStorage.votes);
