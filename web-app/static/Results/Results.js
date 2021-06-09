f0b8f8492bec572b2c8d3a8a620ff30bdc40a5f0
Max <mlm20@ic.ac.uk>
2021-06-09 18:24:31 +0100

    added skeleton

--- web-app/static/Results/Results.js
+++ web-app/static/Results/Results.js
@@ -1,4 +1,16 @@
-import Algorithm from "./RCV_algorithm";
-
+// variables from sessionStorage
 const candidate_list = JSON.parse(sessionStorage.candidate_list);
 const raw_voting_data = JSON.parse(sessionStorage.votes);
+
+// Get document elements
+
+// Algorithm functions from server
+
+
+// Results summary string
+
+
+// Results table
+
+
+// Database storage
