ecf807ce5a6a21f04d5272abb50ad1736a4a31b8
Max <mlm20@ic.ac.uk>
2021-06-09 11:40:54 +0100

    removed debugging tools

--- web-app/static/Results/RCV_algorithm.js
+++ web-app/static/Results/RCV_algorithm.js
@@ -4,35 +4,6 @@
 
 const Algorithm = Object.create(null);
 
-// example data to debug with
-// DELETEME // before commit
-const example_raw_data = [
-    {
-        "blue": 1,
-        "red": 3,
-        "purple": 2,
-        "yellow": 4
-    },
-    {
-        "blue": 2,
-        "red": 3,
-        "purple": 4,
-        "yellow": 1
-    },
-    {
-        "blue": 1,
-        "red": 2,
-        "purple": 4,
-        "yellow": 3
-    },
-    {
-        "blue": 3,
-        "red": 4,
-        "purple": 2,
-        "yellow": 1
-    }
-];
-
 ////
 // Helper Functions
 ////
@@ -252,5 +223,3 @@ Algorithm.how_many_rounds = (raw_data) => Algorithm.results(raw_data).length;
 
 // export module
 export default Object.freeze(Algorithm);
-
-debugger;
