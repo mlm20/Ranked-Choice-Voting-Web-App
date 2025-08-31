4a7959a7a0ee0a945c665814174e8b113c4397cc
Maximilian Matthews <maxmatthews2048@gmail.com>
2025-08-31 21:42:54 +0100

    fix: adapted server.js for vercel deployment

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import Handler from "./handler.js";
 
-const port = 8080;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.use("/", express.static("public"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", express.json());
 app.post("/", function (req, res) {
     const request_object = req.body;
 
-    Handler[request_object.type] (request_object).then(
-        function (response_object) {
-            res.json(response_object);
+    Handler[request_object.type](request_object).then(function (
+        response_object
+    ) {
+        res.json(response_object);
     });
 });
 
-app.listen(port, function () {
-    console.log(`Listening on port ${port} – http://localhost:${port}`);
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
+});
+
+export default app;
